d204d5d3562661d8016e0c1b0bb92bbef7329e05
Paul Csapak <paul@base34.com>
2020-08-04 17:10:31 +0200

    add ftp deploy snippet

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,5 @@
 
-echo "> output test var"
-echo "${FTP_USER}"
-echo "${FTP_PASSWORD}"
-echo "${FTP_URL}"
+echo "> deploy public folder via ftp"
 
-echo $FTP_USER
-echo $FTP_PASSWORD
-echo $FTP_URL
-
-# find . -type f -exec curl -u "${FTP_USER}:${FTP_PASSWORD}" --ftp-create-dirs -T {} ${FTP_URL}/{} \;
+cd tmp-blog/public
+find . -type f -exec curl -u "${FTP_USER}:${FTP_PASSWORD}" --ftp-create-dirs -T {} ${FTP_URL}/{} \;
