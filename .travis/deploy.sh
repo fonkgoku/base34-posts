e76459296782215aa61bc502fd7dbf08fc6fc701
Paul Csapak <paul@base34.com>
2020-08-04 16:49:38 +0200

    output vars

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -4,5 +4,8 @@ echo "${FTP_USER}"
 echo "${FTP_PASSWORD}"
 echo "${FTP_URL}"
 
-#curl -p - --insecure  "ftp://82.45.34.23:21/CurlPutTest/" --user "testuser:testpassword" -T "C:\test\testfile.xml" --ftp-create-dirs
-#curl  -k "sftp://83.46.38.23:22/CurlPutTest/" --user "testuser:testpassword" -T "C:\test\testfile.xml" --ftp-create-dirs
+echo $FTP_USER
+echo $FTP_PASSWORD
+echo $FTP_URL
+
+# find . -type f -exec curl -u "${FTP_USER}:${FTP_PASSWORD}" --ftp-create-dirs -T {} ${FTP_URL}/{} \;
