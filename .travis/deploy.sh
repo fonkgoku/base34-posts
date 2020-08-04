aa2ea4bd3777a732aae9cab70035ecd44ed90dbb
Paul Csapak <paul@base34.com>
2020-08-04 16:23:13 +0200

    test var output

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,7 +1,8 @@
-ls
 
-# echo "> output test var"
-# echo "${TEST_VAR}"
+echo "> output test var"
+echo "${FTP_USER}"
+echo "${FTP_PASSWORD}"
+echo "${FTP_URL}"
 
 #curl -p - --insecure  "ftp://82.45.34.23:21/CurlPutTest/" --user "testuser:testpassword" -T "C:\test\testfile.xml" --ftp-create-dirs
 #curl  -k "sftp://83.46.38.23:22/CurlPutTest/" --user "testuser:testpassword" -T "C:\test\testfile.xml" --ftp-create-dirs
