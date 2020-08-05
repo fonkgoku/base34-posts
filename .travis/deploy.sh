671cd5d4deff9a6cda350d97bf858dbf51527548
Paul Csapak <paul@base34.com>
2020-08-05 13:59:18 +0200

    remove logs

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,19 +1,5 @@
-
 echo "> deploy public folder via ftp"
 
-echo "${FTP_USER}"
-echo "${FTP_PASSWORD}"
-echo "${FTP_URL}"
-
-echo $FTP_USER
-echo $FTP_PASSWORD
-echo $FTP_URL
-
-echo FTP_USER
-echo FTP_PASSWORD
-echo FTP_URL
-
-echo "${FTP_USER}:${FTP_PASSWORD}"
-
 cd tmp-blog/public
+
 find . -type f -exec curl -u "${FTP_USER}:${FTP_PASSWORD}" --ftp-create-dirs -T {} ${FTP_URL}/{} \;
